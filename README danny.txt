150c301b1e2791183703ae079d7741e99e819125
Danny <djm230@case.edu>
2018-02-22 23:23:52 -0500

    main commit and readme
    Signed-off-by: Danny <djm230@case.edu>

---
+++ README danny.txt
@@ -0,0 +1,10 @@
+Danny Miles
+Week of 2/18
+
+2/22: JournalEntry.java:
+	Created skeleton of JournalEntry class, including fields and base methods, as per the design document
+
+
+2/20: Refreshed myself on basics of Android Studio development, researched the utilization of Android system features
+for app use; in particular, the 
+Set up Android Studio development environment, coordinated version control protocols among team members.
--- app/src/main/java/seniorproj/munchbox/JournalEntry.java
+++ app/src/main/java/seniorproj/munchbox/JournalEntry.java
@@ -1,12 +1,45 @@
 package seniorproj.munchbox;
 
+import android.media.Image;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
- * Created by Paul on 2/22/2018.
+ * Created by Paul and Danny on 2/22/2018.
  */
 
 public class JournalEntry
 {
-    /*Heya friends please let me know if this works*/
+  private int identifier; //system-assigned
+    public String nameOfDish;
+    public String restaurantName;
+    public int rating;
+    public Image photo = null;
+    public int frequency;
+    private String description;
+    public ArrayList<String> tags;
+    public Date entryDate = null;
 
-    //Testing again
+    /*Returns all information in the order of: name, restaurant, tags */
+    public ArrayList<String> getKeywords() {
+        ArrayList<String> returnList = new ArrayList<String>();
+        returnList.add(nameOfDish);
+        returnList.add(restaurantName);
+        for (String tag: tags) {
+            returnList.add(tag);
+        }
+        return returnList;
+    }
+
+    public JournalEntry(Image photo) {
+        rating = 0;
+        nameOfDish = "";
+        restaurantName = "";
+        description = "";
+        photo = photo;
+        frequency = 1;
+        entryDate = Calendar.getInstance().getTime();  //generate date on entry creation
+    }
 }
