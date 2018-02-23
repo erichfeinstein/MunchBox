2d3c89e4781c0ebe8f8e21d17d27a03b42a5fba5
Paul <pmp59@case.edu>
2018-02-23 13:16:10 -0500

    Added a bunch of methods to the JournalEntry class, reformatted for private variables. Also created the LinkedList in Main Activity and can now create new entries. Need pictures to do so, probably will remove that for testing purposes.

--- app/src/main/java/seniorproj/munchbox/JournalEntry.java
+++ app/src/main/java/seniorproj/munchbox/JournalEntry.java
@@ -13,33 +13,128 @@ import java.util.Date;
 public class JournalEntry
 {
   private int identifier; //system-assigned
-    public String nameOfDish;
-    public String restaurantName;
-    public int rating;
+    private String nameOfDish;
+    private String restaurantName;
+    private int rating;
     public Image photo = null;
-    public int frequency;
+    private int frequency;
     private String description;
-    public ArrayList<String> tags;
+    private ArrayList<String> tags;
     public Date entryDate = null;
 
     /*Returns all information in the order of: name, restaurant, tags */
-    public ArrayList<String> getKeywords() {
+    public ArrayList<String> getKeywords()
+    {
         ArrayList<String> returnList = new ArrayList<String>();
         returnList.add(nameOfDish);
         returnList.add(restaurantName);
-        for (String tag: tags) {
+        for (String tag: tags)
+        {
             returnList.add(tag);
         }
         return returnList;
     }
 
-    public JournalEntry(Image photo) {
+    public JournalEntry(Image photo)
+    {
         rating = 0;
         nameOfDish = "";
         restaurantName = "";
+        //restaurantName = GoogleFindRestaurantName();
+        //tags.add(GoogleFindTags);
         description = "";
-        photo = photo;
+        this.photo = photo;
         frequency = 1;
         entryDate = Calendar.getInstance().getTime();  //generate date on entry creation
+    }
+
+    //These methods will used to change the information in each datapoint.
+    //They will be used both for the first time entering and for editting existing datapoints.
+    public void incrementFrequency()
+    {
+        frequency += 1;
+    }
+
+    public int getFrequency()
+    {
+        return frequency;
+    }
+
+    public void rateDish(int newRating)
+    {
+        if(newRating < 0)
+        {
+            rating = 0;
+        }
+        else if(newRating > 10)
+        {
+            rating = 10;
+        }
+        else
+        {
+            rating = newRating;
+        }
+    }
+
+    public int getRating()
+    {
+        return rating;
+    }
+
+    public void renameDish(String newName)
+    {
+        nameOfDish = newName;
+    }
+
+    public String getNameOfDish()
+    {
+        return nameOfDish;
+    }
+
+    public void newDescription(String newDescription)
+    {
+        description = newDescription;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    //This one is for changing the name of the restaurant. Ideally, since Google will determine the restaurant, this will never be used.
+    //Giving users the option is important, however.
+    public void changeRestaurantName(String newName)
+    {
+        restaurantName = newName;
+    }
+
+    public String getRestaurantName()
+    {
+        return restaurantName;
+    }
+
+    public void newTag(String newTag)
+    {
+        tags.add(newTag);
+    }
+
+    public void removeTag(int tagToDrop)
+    {
+        if(tagToDrop >= 0 && tagToDrop < tags.size())
+        {
+            tags.remove(tagToDrop);
+        }
+    }
+
+    public String GoogleFindRestaurantName()
+    {
+        //Whatever API magic needs to happen in here
+        return null;
+    }
+
+    public ArrayList<String> GoogleFindTags()
+    {
+        //Whatever API magic needs to happen in here
+        return null;
     }
 }
--- app/src/main/java/seniorproj/munchbox/MainActivity.java
+++ app/src/main/java/seniorproj/munchbox/MainActivity.java
@@ -1,13 +1,28 @@
 package seniorproj.munchbox;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
 
+    LinkedList<JournalEntry> Journal = new LinkedList<JournalEntry>();
+    public Image newEntryPhoto = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void createEntry(Image newEntryPhoto)
+    {
+        Journal.add(new JournalEntry(newEntryPhoto));
+    }
+
+
 }
