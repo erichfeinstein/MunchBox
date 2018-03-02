39063e157c8e7c1b47f7d18f5c1e000834aa90d1
jxy367 <jxy367@case.edu>
2018-03-02 09:52:38 -0500

    Renamed mapsRequest to DistanceMatrixRequest. Added LocationGetter (includes listener to gather current location).

--- app/src/main/java/seniorproj/munchbox/DistanceMatrixRequest.java
+++ app/src/main/java/seniorproj/munchbox/DistanceMatrixRequest.java
@@ -11,19 +11,26 @@ import android.util.JsonReader;
 
 import java.io.*;
 import java.net.*;
-
-public class MapsRequest extends AsyncTask<URL, Integer, Long> {
+/*
+Takes URLs and gets JSON object. Exact JSON parsing to be decided on later.
+doInBackground is generic and will be used again later.
+For our purposes, the URLs should be properly formatted google distance matrix urls.
+ */
+public class DistanceMatrixRequest extends AsyncTask<URL, Integer, Long> { //AKA "Roll for Charisma"
     @Override
     protected Long doInBackground(URL... urls) {
         long completed;
         for (completed = 0; completed<urls.length; completed++) {
-            System.out.println(completed);
             request(urls[(int)completed]);
             if (isCancelled()) break;
         }
         return completed;
     }
 
+    /*
+    Sends an https request and gets back JSON reader.
+    THIS NEEDS TO BE UPDATED FOR ACTUAL DATA GATHERING
+     */
     private void request(URL url){
         try {
             URL u = url;
---
+++ app/src/main/java/seniorproj/munchbox/LocationGetter.java
@@ -0,0 +1,64 @@
+package seniorproj.munchbox;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+/**
+ * Created by Jxy367 on 3/2/2018.
+ */
+
+
+/*
+Testing necessary for this one.
+ */
+public class LocationGetter {
+    private Context context;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    public LocationGetter(Context mContext){
+        context = mContext;
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {}
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        if(context.checkPermission(Context.LOCATION_SERVICE, android.os.Process.myPid(), android.os.Process.myUid())
+            != PackageManager.PERMISSION_GRANTED) {
+
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    public Location getLocation(){
+        if(context.checkPermission(Context.LOCATION_SERVICE, android.os.Process.myPid(), android.os.Process.myUid())
+                != PackageManager.PERMISSION_GRANTED){
+            /*
+            Need to ask for permission and then recall this function.
+             */
+            return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        }
+        else{
+            return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+
+    }
+
+}
