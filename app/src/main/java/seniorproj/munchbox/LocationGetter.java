77c93d1cdc64371c5094833aeee1452b9ece5ebe
jxy367 <jxy367@case.edu>
2018-03-24 00:29:23 -0400

    Implemented getting of current location.

--- app/src/main/java/seniorproj/munchbox/LocationGetter.java
+++ app/src/main/java/seniorproj/munchbox/LocationGetter.java
@@ -6,6 +6,20 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.PlacesOptions;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 /**
  * Created by Jxy367 on 3/2/2018.
@@ -64,5 +78,30 @@ public class LocationGetter {
 
     }
 
+    public void googleMapLocation(Location location){
+        PlaceDetectionClient p = Places.getPlaceDetectionClient(context);
+        if(context.checkPermission(Context.LOCATION_SERVICE, android.os.Process.myPid(), android.os.Process.myUid())
+                != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("sigh...");
+        }
+        else {
+                Task<PlaceLikelihoodBufferResponse> placeResult = p.getCurrentPlace(null);
+            placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                @Override
+                public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                    PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
 
+                    /*
+                    Data storage
+                    Use placeLikelihood.getPlace().otherFunction() for data
+                    Talk to Eric about format.
+                     */
+
+                    //for (PlaceLikelihood placeLikelihood : likelyPlaces) {}
+                    //likelyPlaces.release();
+                }
+            });
+        }
+
+    }
 }
